4bcc180e29432e34ce7826f288a10ed13e78fdc5
Doosan <emptyfridge0900@gmail.com>
2024-11-20 08:38:27 -0500

    update

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,13 @@ use axum::{
     Router,
 };
 use axum_extra::TypedHeader;
+use tokio_util::codec::{Framed, LinesCodec};
 
 use std::{
-    borrow::{BorrowMut, Cow},
-    cell::RefCell,
-    collections::{HashMap, HashSet},
-    ops::Deref,
+    collections::HashMap,
+    error::Error,
+    io,
+    sync::{mpsc, Mutex},
 };
 use std::{net::SocketAddr, path::PathBuf};
 use std::{ops::ControlFlow, sync::Arc};
@@ -27,7 +28,7 @@ use axum::extract::connect_info::ConnectInfo;
 use axum::extract::ws::CloseFrame;
 
 //allows to split the websocket stream into separate TX and RX branches
-use futures::{lock::Mutex, sink::SinkExt, stream::StreamExt};
+use futures::{sink::SinkExt, stream::StreamExt};
 use futures_util::stream::{SplitSink, SplitStream};
 
 //#[derive(Clone)]
@@ -35,34 +36,8 @@ struct AppState {
     rooms: Mutex<HashMap<String, Room>>,
 }
 use chrono::prelude::*;
-use tokio::{net::TcpListener, sync::broadcast};
+use tokio::net::{tcp::OwnedWriteHalf, TcpListener, TcpStream};
 
-struct Room {
-    users: Mutex<HashSet<String>>,
-    history: Vec<ChatMessage>,
-    tx: Mutex<Vec<MessageSender>>,
-}
-impl Room {
-    fn new() -> Self {
-        Self {
-            users: Mutex::new(HashSet::new()),
-            history: vec![],
-            tx: Mutex::new(vec![]),
-        }
-    }
-}
-struct ChatMessage {
-    text: String,
-    sedner_id: i32,
-    receiver_id: i32,
-    time: DateTime<Utc>,
-}
-enum MessageSender {
-    WebSocket(broadcast::Sender<String>),
-    TcpSocket(TcpListener),
-}
-//unsafe impl Send for AppState {}
-//unsafe impl Sync for AppState {}
 #[tokio::main]
 async fn main() {
     tracing_subscriber::registry()
@@ -76,16 +51,39 @@ async fn main() {
 
     let assets_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("assets");
 
+    let state = Arc::new(AppState {
+        rooms: Mutex::new(HashMap::new()),
+    });
     let app = Router::new()
         .fallback_service(ServeDir::new(assets_dir).append_index_html_on_directories(true))
         .route("/ws", any(ws_handler))
+        .route("/tcp", any(tcp_handler))
         .layer(
             TraceLayer::new_for_http()
                 .make_span_with(DefaultMakeSpan::default().include_headers(true)),
         )
-        .with_state(Arc::new(AppState {
-            rooms: Mutex::new(HashMap::new()),
-        }));
+        .with_state(Arc::clone(&state));
+
+    let listener1 = tokio::net::TcpListener::bind("127.0.0.1:3001")
+        .await
+        .unwrap();
+    tokio::spawn(async move {
+        loop {
+            // Asynchronously wait for an inbound TcpStream.
+            let (stream, addr) = listener1.accept().await?;
+
+            // Clone a handle to the `Shared` state for the new connection.
+            let state = Arc::clone(&state);
+
+            // Spawn our handler to be run asynchronously.
+            tokio::spawn(async move {
+                tracing::debug!("accepted connection");
+                if let Err(e) = process(state, stream, addr).await {
+                    tracing::info!("an error occurred; error = {:?}", e);
+                }
+            });
+        }
+    });
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
@@ -99,6 +97,66 @@ async fn main() {
     .unwrap();
 }
 
+/// Shorthand for the transmit half of the message channel.
+type Tx = mpsc::Sender<String>;
+
+/// Shorthand for the receive half of the message channel.
+type Rx = mpsc::Receiver<String>;
+
+/// The state for each connected client.
+struct Peer {
+    rx: Rx,
+}
+
+impl Peer {
+    /// Create a new instance of `Peer`.
+    fn new(addr: SocketAddr, room_name: &str, state: Arc<AppState>) -> io::Result<Peer> {
+        // Create a channel for this peer
+        let (tx, rx) = mpsc::channel::<String>();
+
+        // Add an entry for this `Peer` in the shared state map.
+        let mut rooms = state.rooms.lock().unwrap();
+        let room = rooms.get_mut(room_name).unwrap();
+        room.peers.insert(addr, tx);
+
+        Ok(Peer { rx })
+    }
+}
+
+struct Room {
+    peers: HashMap<SocketAddr, Tx>,
+    history: Vec<ChatMessage>,
+    //tx: Arc<Mutex<Vec<MessageSender>>>,
+}
+impl Room {
+    fn new() -> Self {
+        Self {
+            peers: HashMap::new(),
+            history: vec![],
+            //tx: Arc::new(Mutex::new(vec![])),
+        }
+    }
+    fn broadcast(&mut self, sender: SocketAddr, message: &str) {
+        for peer in self.peers.iter_mut() {
+            if *peer.0 != sender {
+                let _ = peer.1.send(message.into());
+            }
+        }
+    }
+}
+struct ChatMessage {
+    text: String,
+    sedner_id: i32,
+    receiver_id: i32,
+    time: DateTime<Utc>,
+}
+#[derive(Debug)]
+enum MessageSender {
+    WebSocket(SplitSink<WebSocket, Message>),
+    TcpSocket(OwnedWriteHalf),
+}
+
+async fn tcp_handler(State(state): State<Arc<AppState>>) {}
 async fn ws_handler(
     ws: WebSocketUpgrade,
     user_agent: Option<TypedHeader<headers::UserAgent>>,
@@ -118,87 +176,36 @@ async fn ws_handler(
 
 /// Actual websocket statemachine (one will be spawned per connection)
 async fn handle_socket(mut socket: WebSocket, who: SocketAddr, state: Arc<AppState>) {
-    // send a ping (unsupported by some browsers) just to kick things off and get a response
-    if socket.send(Message::Ping(vec![1, 2, 3])).await.is_ok() {
-        println!("Pinged {who}...");
-    } else {
-        println!("Could not send ping {who}!");
-        // no Error here since the only thing we can do is to close the connection.
-        // If we can not send messages, there is no way to salvage the statemachine anyway.
-        return;
-    }
-
-    // receive single message from a client (we can either receive or send with socket).
-    // this will likely be the Pong for our Ping or a hello message from client.
-    // waiting for message from a client will block this task, but will not block other client's
-    // connections.
-    if let Some(msg) = socket.recv().await {
-        if let Ok(msg) = msg {
-            if process_message(msg, who).is_break() {
-                return;
-            }
-        } else {
-            println!("client {who} abruptly disconnected");
-            return;
-        }
-    }
-
-    // Since each client gets individual statemachine, we can pause handling
-    // when necessary to wait for some external event (in this case illustrated by sleeping).
-    // Waiting for this client to finish getting its greetings does not prevent other clients from
-    // connecting to server and receiving their greetings.
-    for i in 1..5 {
-        if socket
-            .send(Message::Text(format!("Hi {i} times!")))
-            .await
-            .is_err()
-        {
-            println!("client {who} abruptly disconnected");
-            return;
-        }
-        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
-    }
-
-    // By splitting socket we can send and receive at the same time. In this example we will send
     // unsolicited messages to client based on some sort of server's internal event (i.e .timer).
     let (mut sender, mut receiver) = socket.split();
 
-    while let Some(Ok(msg)) = receiver.next().await {
-        match msg {
-            Message::Text(t) => println!(""),
-            Message::Binary(d) => {}
-            Message::Ping(ping) => {}
-            Message::Pong(pong) => {}
-            Message::Close(c) => {}
-        }
-    }
-
     {
-        let sender_list = state.senders.lock().await;
-        println!("{}", sender_list.len());
+        let mut rooms = state.rooms.lock().unwrap();
+        let room = rooms
+            .entry("chatroom".to_string())
+            .or_insert_with(Room::new);
     }
 
-    let cloned = state.senders.clone();
+    let mut peer = Peer::new(who, "chatroom", state.clone());
+
+    let peer = peer.unwrap();
+    let state1 = Arc::clone(&state);
+    let state2 = Arc::clone(&state);
+
     // Spawn a task that will push several messages to the client (does not matter what client does)
     let mut send_task = tokio::spawn(async move {
-        {
-            println!("{}", cloned.lock().await.len());
-        }
-
-        let mut cnt = 0;
         while let Some(Ok(msg)) = receiver.next().await {
             match msg {
                 Message::Text(t) => {
                     println!(">>> {who} sent str: {t:?}");
                     {
-                        let mut xxx = cloned.lock().await;
-                        for item in xxx.iter_mut() {
-                            if item.send(Message::Text(t.clone())).await.is_err() {
-                                println!("Erroorrr!");
-                            }
-                        }
+                        let rooms = state1.rooms.lock();
+                        let mut rooms = rooms.unwrap();
+                        let room = rooms.get_mut("chatroom").unwrap();
+                        room.broadcast(who, &t);
                     }
                 }
+
                 Message::Binary(d) => {
                     println!(">>> {} sent {} bytes: {:?}", who, d.len(), d);
                 }
@@ -242,17 +249,25 @@ async fn handle_socket(mut socket: WebSocket, who: SocketAddr, state: Arc<AppSta
     });
 
     // This second task will receive messages from client and print them on server console
-    // let mut recv_task = tokio::spawn(async move {
-    //     let mut cnt = 0;
-    //     while let Some(Ok(msg)) = receiver.next().await {
-    //         cnt += 1;
-    //         // print message and break if instructed to do so
-    //         if process_message(msg, who).is_break() {
-    //             break;
-    //         }
-    //     }
-    //     cnt
-    // });
+    let mut recv_task = tokio::spawn(async move {
+        // let mut cnt = 0;
+        // while let Some(Ok(msg)) = receiver.next().await {
+        //     cnt += 1;
+        //     // print message and break if instructed to do so
+        //     if process_message(msg, who).is_break() {
+        //         break;
+        //     }
+        // }
+        // cnt
+        while let Ok(msg) = peer.rx.recv() {
+            println!("Got a message: {}", msg);
+            if sender.send(Message::Text(msg)).await.is_err() {
+                println!("Sending message failed");
+                break;
+            }
+        }
+        0
+    });
 
     // If any one of the tasks exit, abort the other.
     tokio::select! {
@@ -261,19 +276,27 @@ async fn handle_socket(mut socket: WebSocket, who: SocketAddr, state: Arc<AppSta
                 Ok(a) => println!(" messages sent to {who}"),
                 Err(a) => println!("Error sending messages {a:?}")
             }
-            //recv_task.abort();
+            recv_task.abort();
+
         },
-        // rv_b = (&mut recv_task) => {
-        //     match rv_b {
-        //         Ok(b) => println!("Received {b} messages"),
-        //         Err(b) => println!("Error receiving messages {b:?}")
-        //     }
-        //     send_task.abort();
-        // }
+        rv_b = (&mut recv_task) => {
+            match rv_b {
+                Ok(b) => println!("Received {b} messages"),
+                Err(b) => println!("Error receiving messages {b:?}")
+            }
+            send_task.abort();
+        }
     }
 
     // returning from the handler closes the websocket connection
     println!("Websocket context {who} destroyed");
+
+    {
+        let mut room = state2.rooms.lock().unwrap();
+        let room = room.get_mut("chatroom").unwrap();
+        room.peers.remove(&who);
+        println!("{} peer in the room", room.peers.len());
+    }
 }
 
 fn process_message(msg: Message, who: SocketAddr) -> ControlFlow<(), ()> {
@@ -307,4 +330,96 @@ fn process_message(msg: Message, who: SocketAddr) -> ControlFlow<(), ()> {
         }
     }
     ControlFlow::Continue(())
+}
+
+async fn process(
+    state: Arc<AppState>,
+    stream: TcpStream,
+    addr: SocketAddr,
+) -> Result<(), Box<dyn Error>> {
+    let mut lines = Framed::new(stream, LinesCodec::new());
+
+    // Send a prompt to the client to enter their username.
+    lines.send("Please enter your username:").await?;
+
+    // Read the first line from the `LineCodec` stream to get the username.
+    let username = match lines.next().await {
+        Some(Ok(line)) => line,
+        // We didn't get a line so we return early here.
+        _ => {
+            tracing::error!("Failed to get username from {}. Client disconnected.", addr);
+            return Ok(());
+        }
+    };
+
+    {
+        let mut rooms = state.rooms.lock().unwrap();
+        let room = rooms
+            .entry("chatroom".to_string())
+            .or_insert_with(Room::new);
+    }
+
+    let mut peer = Peer::new(addr, "chatroom", state.clone());
+
+    let peer = peer.unwrap();
+    let state1 = Arc::clone(&state);
+    let state2 = Arc::clone(&state);
+    // Register our peer with state which internally sets up some channels.
+    let mut peer = Peer::new(addr, "chatroom", state.clone())?;
+
+    // A client has connected, let's let everyone know.
+    // {
+    //     let mut state = state.lock().unwrap();
+    //     let msg = format!("{} has joined the chat", username);
+    //     tracing::info!("{}", msg);
+    //     state.broadcast(addr, &msg).await;
+    // }
+
+    // Process incoming messages until our stream is exhausted by a disconnect.
+    loop {
+        tokio::select! {
+            // A message was received from a peer. Send it to the current user.
+            // Some(msg) = peer.rx.recv() => {
+            //     peer.lines.send(&msg).await?;
+            // }
+            result = lines.next() => match result {
+                // A message was received from the current user, we should
+                // broadcast this message to the other users.
+                Some(Ok(msg)) => {
+                    // let mut state = state.lock().unwrap();
+                    // let msg = format!("{}: {}", username, msg);
+
+                    // state.broadcast(addr, &msg).await;
+
+                    let rooms = state1.rooms.lock();
+                    let mut rooms = rooms.unwrap();
+                    let room = rooms.get_mut("chatroom").unwrap();
+                    room.broadcast(addr, &msg);
+                }
+                // An error occurred.
+                Some(Err(e)) => {
+                    tracing::error!(
+                        "an error occurred while processing messages for {}; error = {:?}",
+                        username,
+                        e
+                    );
+                }
+                // The stream has been exhausted.
+                None => break,
+            },
+        }
+    }
+
+    // If this section is reached it means that the client was disconnected!
+    // Let's let everyone still connected know about it.
+    {
+        let mut state = state.lock().unwrap();
+        state.peers.remove(&addr);
+
+        let msg = format!("{} has left the chat", username);
+        tracing::info!("{}", msg);
+        state.broadcast(addr, &msg).await;
+    }
+
+    Ok(())
 }
